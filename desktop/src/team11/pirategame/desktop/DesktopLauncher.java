9ab8ef31a1d103ad03ed271e7f69e361400843b2
Harry Ryan <harryryan2012@gmail.com>
2021-12-02 11:48:27 +0000

    Initial LibGDX implementation

---
+++ desktop/src/team11/pirategame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package team11.pirategame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import team11.pirategame.PirateGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PirateGame(), config);
+	}
+}
