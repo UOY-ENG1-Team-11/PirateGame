fbbeea5c3f98198e40254fd20030928fd9952026
Harry Ryan <harryryan2012@gmail.com>
2021-12-29 22:48:25 +0000

    Movement
    First draft of basic movement for the ship

--- core/src/team11/pirategame/PirateGame.java
+++ core/src/team11/pirategame/PirateGame.java
@@ -1,31 +1,117 @@
 package team11.pirategame;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+import com.badlogic.gdx.math.Polygon;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class PirateGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	
+	private OrthographicCamera camera;
+	private SpriteBatch batch;
+	private BitmapFont font;
+	private ShapeRenderer shapeRenderer;
+	//if these values are changed, change in desktop launcher too
+	private int screenWidth = 1920;
+	private int screenHeight = 1080;
+	
+	private Polygon player;
+	private double playerX;
+	private double playerY;
+	private double playerRotation;
+	private int playerWidth = 64;
+	private int playerHeight = 128;
+	//these values are in pixels/second
+	private double playerSpeed = 0;
+	private double playerAccel = 100;
+	private double playerNaturalDecel = 50;
+	private double playerBrakeDecel = 250;
+	private double playerSpeedCap = 250;
+	private double playerMinusSpeedCap = -50;
+	private double playerRotSpeed = 120;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		camera = new OrthographicCamera();
+		font = new BitmapFont();
+		shapeRenderer = new ShapeRenderer();
+		camera.setToOrtho(false, screenWidth, screenHeight);
+		player = new Polygon(new float[]{0,0,playerWidth,0,playerWidth,playerHeight,0,playerHeight});
+		player.setOrigin(64/2, 128/2);
+		player.setPosition(screenWidth/2 - playerWidth/2, screenHeight/2 - playerHeight/2);
+		playerX = screenWidth/2 - playerWidth/2;
+		playerY = screenHeight/2 - playerHeight/2;
+		playerRotation = 0;
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(0, 0.4f, 0.6f, 1);
+		camera.update();
+		batch.setProjectionMatrix(camera.combined);
+		shapeRenderer.setProjectionMatrix(camera.combined);
+		shapeRenderer.begin(ShapeType.Line);
+        shapeRenderer.setColor(Color.BROWN);
+        shapeRenderer.polygon(player.getTransformedVertices());
+        shapeRenderer.end();
 		batch.begin();
-		batch.draw(img, 0, 0);
+		font.draw(batch, "SPEED: " + playerSpeed, 20, 20);
+		// IMAGE PROCESSING HERE
 		batch.end();
+		playerMovement();
+		cameraMovement();
+	}
+	
+	private void playerMovement() {
+		if(Gdx.input.isKeyPressed(Input.Keys.W)) {
+			if(playerSpeed < playerSpeedCap) {
+				playerSpeed += playerAccel * Gdx.graphics.getDeltaTime();
+				if(playerSpeed > playerSpeedCap) {
+					playerSpeed = playerSpeedCap;
+				}
+			}
+		} else if(Gdx.input.isKeyPressed(Input.Keys.S)) {
+			playerSpeed -= playerBrakeDecel * Gdx.graphics.getDeltaTime();
+			if(playerSpeed < playerMinusSpeedCap) {
+				playerSpeed = playerMinusSpeedCap;
+			}
+		} else if(playerSpeed > 0){
+				playerSpeed -= playerNaturalDecel * Gdx.graphics.getDeltaTime();
+				if(playerSpeed < 0) {
+					playerSpeed = 0;
+				}
+		} else if(playerSpeed < 0) {
+			playerSpeed += playerNaturalDecel * Gdx.graphics.getDeltaTime();
+			if(playerSpeed > 0) {
+				playerSpeed = 0;
+			}
+		}
+		if(playerSpeed != 0) {
+			playerX += playerSpeed * Math.sin(Math.toRadians(-player.getRotation())) * Gdx.graphics.getDeltaTime();
+			playerY += playerSpeed * Math.cos(Math.toRadians(-player.getRotation())) * Gdx.graphics.getDeltaTime();
+			player.setPosition(Math.round(playerX), Math.round(playerY));
+			if(Gdx.input.isKeyPressed(Input.Keys.A)) player.rotate((float)(playerRotSpeed * Math.abs(playerSpeed/playerSpeedCap) * Gdx.graphics.getDeltaTime()));
+			if(Gdx.input.isKeyPressed(Input.Keys.D)) player.rotate((float)(-playerRotSpeed * Math.abs(playerSpeed/playerSpeedCap) * Gdx.graphics.getDeltaTime()));
+		}
+	}
+	
+	private void cameraMovement() {
+		
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		font.dispose();
+		shapeRenderer.dispose();
 	}
 }
--- desktop/src/team11/pirategame/desktop/DesktopLauncher.java
+++ desktop/src/team11/pirategame/desktop/DesktopLauncher.java
@@ -7,6 +7,10 @@ import team11.pirategame.PirateGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Pirate Game";
+		//if these values are changed, change in main class too
+		config.width = 1920;
+		config.height = 1080;
 		new LwjglApplication(new PirateGame(), config);
 	}
 }
